657c4d7da2672d456b32c85083cddb2a40012be0
coderkeke <18290060679@163.com>
2020-08-26 19:41:53 +0800

    复习第一天

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,24 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    name:"Coderkeke",
+    age:18,
+    students:[
+      {id:110,name:'小明',age:12},
+      {id:111,name:'效力',age:23},
+      {id:112,name:'下手',age:31}
+    ],
+    counter:0
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleBtnClick(){
+    this.setData({
+      counter:this.data.counter +1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSubtraction(){
+    this.setData({
+      counter: this.data.counter - 1
+    })
   }
 })
