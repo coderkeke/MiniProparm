19564446e2cf99b4367e665262c51a2f6e4241f0
coderkeke <18290060679@163.com>
2020-08-29 14:33:18 +0800

    初始化项目

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,24 +1,2 @@
 // pages/home/home.js
-Page({
-  data:{
-    name:"Coderkeke",
-    age:18,
-    students:[
-      {id:110,name:'小明',age:12},
-      {id:111,name:'效力',age:23},
-      {id:112,name:'下手',age:31}
-    ],
-    counter:0
-  },
-
-  handleBtnClick(){
-    this.setData({
-      counter:this.data.counter +1
-    })
-  },
-  handleSubtraction(){
-    this.setData({
-      counter: this.data.counter - 1
-    })
-  }
-})
+Page({})
