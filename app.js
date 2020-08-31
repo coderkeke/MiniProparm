87a77a0a6d452b409af668f3874c0fe911e7f7f6
coderkeke <18290060679@163.com>
2020-08-31 18:18:50 +0800

    登录复习

---
+++ app.js
@@ -0,0 +1,66 @@
+const TOKEN = "token"
+
+App({
+
+  globalData:{
+    token:""
+  },
+
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
+  onLaunch: function() {
+    const token = wx.getStorageSync(TOKEN)
+    if (token){
+
+
+    }else{
+      this.login()
+    }
+
+  
+  },
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function(options) {
+
+  },
+
+  /**
+   * 当小程序从前台进入后台，会触发 onHide
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+   */
+  onError: function(msg) {
+
+  },
+
+  login(){
+    /**登录操作 code只有5分钟有效期*/
+    wx.login({
+      success: res => {
+        const code = res.code;
+        // wx.request({
+        //   url: 'http://123.207.32.32:3000/login',
+        //   method: "POST",
+        //   data: {
+        //     code
+        //   },
+        //   success: res => {
+        //     const token = res.data.token
+        //     this.globalData.token = token
+        //     wx.setStorageSync(TOKEN, token)
+        //   }
+        // })
+
+      }
+    })
+  }
+})
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,2 +1,17 @@
 // pages/home/home.js
-Page({})
+const app = getApp()
+
+
+Page({
+
+  handleShowToast(){
+    wx.showToast({
+      title: '加载中',
+      duration:10000,
+      icon:"loading"
+    })
+  },
+  onShareAppMessage(options){
+    console.log(options)
+  }
+})
